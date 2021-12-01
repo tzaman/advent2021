5c1d61bd79abf970dc58a2eaf61a9e6d365e948b
Talha Zaman <talha@meshcapade.com>
2021-12-01 19:05:19 +0100

    some cleanup

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -2,26 +2,29 @@ use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
+const INPUT: &str = "src/input/day01/input.txt";
+
 struct Window<const SIZE: usize> {
     values: VecDeque<u32>,
 }
 
 impl<const SIZE: usize> Window<SIZE> {
+    fn new() -> Window<SIZE> {
+        Window {
+            values: VecDeque::new(),
+        }
+    }
+
     fn push_and_check(&mut self, value: u32) -> u32 {
         if self.values.len() < SIZE {
             self.values.push_back(value);
-            0
-        } else {
-            let prev_sum: u32 = self.values.iter().sum();
-            self.values.pop_front();
-            self.values.push_back(value);
-            let new_sum: u32 = self.values.iter().sum();
-            if new_sum > prev_sum {
-                1
-            } else {
-                0
-            }
+            return 0;
         }
+        let prev_sum: u32 = self.values.iter().sum();
+        self.values.pop_front();
+        self.values.push_back(value);
+        let new_sum: u32 = self.values.iter().sum();
+        (new_sum > prev_sum) as u32
     }
 }
 
@@ -50,24 +53,17 @@ fn count_increases(depths: Vec<u32>) -> u32 {
 }
 
 fn count_increases_windowed(depths: Vec<u32>) -> u32 {
-    let mut window = Window::<3> {
-        values: VecDeque::new(),
-    };
-    let mut increasing = 0;
-    for depth in depths {
-        increasing += window.push_and_check(depth);
-    }
-    increasing
+    let mut window = Window::<3>::new();
+    depths
+        .iter()
+        .map(|&depth| window.push_and_check(depth))
+        .sum()
 }
 
 fn main() {
-    println!("Hello, world!");
+    let inc = count_increases(read_input(INPUT));
+    println!("Depth increases: {}", inc);
 
-    let depths = read_input("src/input/day01/input.txt");
-    let inc = count_increases(depths);
-    println!("Increases: {}", inc);
-
-    let depths = read_input("src/input/day01/input.txt");
-    let inc = count_increases_windowed(depths);
+    let inc = count_increases_windowed(read_input(INPUT));
     println!("Windowed increases: {}", inc);
 }
