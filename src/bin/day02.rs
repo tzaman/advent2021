f283ec0fe77edee11ed1de812a70544318ad53f3
Talha Zaman <talha@meshcapade.com>
2021-12-02 21:11:38 +0100

    now with custom enum and error types

--- src/bin/day02.rs
+++ src/bin/day02.rs
@@ -1,65 +1,91 @@
 use advent::{get_my_lines, iter_lines};
 use regex::Regex;
+use std::str::FromStr;
 
 #[derive(Debug)]
-struct Position{
+struct Position {
     x: u32,
     y: u32,
     aim: u32,
 }
 
-impl Position{
+#[derive(Debug)]
+enum Direction {
+    Forward(u32),
+    Up(u32),
+    Down(u32),
+}
+
+#[derive(Debug, Clone)]
+struct AdventError;
+
+impl From<regex::Error> for AdventError {
+    fn from(_: regex::Error) -> AdventError {
+        AdventError
+    }
+}
+
+impl From<std::num::ParseIntError> for AdventError {
+    fn from(_: std::num::ParseIntError) -> AdventError {
+        AdventError
+    }
+}
+
+impl Position {
     fn new() -> Position {
-        Position{ x: 0, y: 0, aim: 0}
+        Position { x: 0, y: 0, aim: 0 }
     }
 
-    fn update(&mut self, command: &str) {
-        let re = Regex::new(r"(forward|up|down) (\d+)").unwrap();
-        let cap = re.captures(command).expect("Failed to parse line.");
-        let direction = cap.get(1).unwrap().as_str();
-        let units = cap.get(2).unwrap().as_str().parse::<u32>().unwrap();
-        match direction {
-            "forward" => self.x += units,
-            "up" => self.y -= units,
-            "down" => self.y += units,
-            _ => panic!("Unknown direction."),
-        }
+    fn update(&mut self, dir: &Direction) {
+        match dir {
+            Direction::Forward(units) => self.x += units,
+            Direction::Up(units) => self.y -= units,
+            Direction::Down(units) => self.y += units,
+        };
     }
 
-    fn update_better(&mut self, command: &str) {
-        let re = Regex::new(r"(forward|up|down) (\d+)").unwrap();
-        let cap = re.captures(command).expect("Failed to parse line.");
-        let direction = cap.get(1).unwrap().as_str();
-        let units = cap.get(2).unwrap().as_str().parse::<u32>().unwrap();
-        match direction {
-            "forward" => {
+    fn update_better(&mut self, dir: &Direction) {
+        match dir {
+            Direction::Forward(units) => {
                 self.x += units;
                 self.y += units * self.aim;
-            },
-            "up" => self.aim -= units,
-            "down" => self.aim += units,
-            _ => panic!(),
+            }
+            Direction::Up(units) => self.aim -= units,
+            Direction::Down(units) => self.aim += units,
+        };
+    }
+}
+
+impl FromStr for Direction {
+    type Err = AdventError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"(forward|up|down) (\d+)")?;
+        let cap = re.captures(s).ok_or(AdventError)?;
+        let direction = cap.get(1).ok_or(AdventError)?.as_str();
+        let units = cap.get(2).ok_or(AdventError)?.as_str().parse::<u32>()?;
+        match direction {
+            "forward" => Ok(Self::Forward(units)),
+            "up" => Ok(Self::Up(units)),
+            "down" => Ok(Self::Down(units)),
+            _ => Err(AdventError),
         }
     }
 }
 
 fn follow_directions() -> u32 {
     let mut pos = Position::new();
-    get_my_lines!().for_each(
-        |line| pos.update(&line)
-    );
+    get_my_lines!().for_each(|line| pos.update(&line.parse::<Direction>().unwrap()));
     pos.x * pos.y
 }
 
 fn follow_directions_better() -> u32 {
     let mut pos = Position::new();
-    get_my_lines!().for_each(
-        |line| pos.update_better(&line)
-    );
+    get_my_lines!().for_each(|line| pos.update_better(&line.parse::<Direction>().unwrap()));
     pos.x * pos.y
 }
 
 fn main() {
     println!("Final multiple: {}", follow_directions());
-    println!("Better multiple: {}", follow_directions_better());
+    println!("Aimed multiple: {}", follow_directions_better());
 }
