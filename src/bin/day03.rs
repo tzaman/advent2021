20b95383d0dd88787b9d13c776260081afabe928
Talha Zaman <talha@meshcapade.com>
2021-12-06 00:35:34 +0100

    day3 cleanup

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -2,16 +2,12 @@ use advent::{get_my_lines, iter_lines};
 use anyhow::{bail, Context, Result};
 use counter::Counter;
 
-#[derive(Debug)]
-struct FreqCounter {
-    size: usize,
-    freq: Vec<Counter<char>>,
-}
-
 fn mcb_at(report: &Vec<String>, pos: usize) -> Result<u8> {
-    let f: Vec<u8> = report.iter().map(|s| s.as_bytes()[pos]).collect();
+    let f: Option<Vec<&u8>> = report.iter().map(|s| s.as_bytes().get(pos)).collect();
     let mut counter: Counter<u8> = Counter::new();
-    f.iter().for_each(|ch| counter[&ch] += 1);
+    f.context("Out of bounds getting MCB")?
+        .iter()
+        .for_each(|ch| counter[&ch] += 1);
     if counter[&b'1'] >= counter[&b'0'] {
         Ok(b'1')
     } else {
@@ -20,9 +16,11 @@ fn mcb_at(report: &Vec<String>, pos: usize) -> Result<u8> {
 }
 
 fn lcb_at(report: &Vec<String>, pos: usize) -> Result<u8> {
-    let f: Vec<u8> = report.iter().map(|s| s.as_bytes()[pos]).collect();
+    let f: Option<Vec<&u8>> = report.iter().map(|s| s.as_bytes().get(pos)).collect();
     let mut counter: Counter<u8> = Counter::new();
-    f.iter().for_each(|ch| counter[&ch] += 1);
+    f.context("Out of bounds getting LCB")?
+        .iter()
+        .for_each(|ch| counter[ch] += 1);
     if counter[&b'0'] <= counter[&b'1'] {
         Ok(b'0')
     } else {
@@ -30,59 +28,27 @@ fn lcb_at(report: &Vec<String>, pos: usize) -> Result<u8> {
     }
 }
 
-fn mcb(fc: &Counter<char>) -> Result<char> {
-    Ok(fc
-        .most_common()
-        .first()
-        .context("Failed to fetch most common element!")?
-        .0)
-}
-
-fn lcb(fc: &Counter<char>) -> Result<char> {
-    Ok(fc
-        .most_common()
-        .last()
-        .context("Failed to fetch least common element!")?
-        .0)
-}
-
 fn binary_to_int(s: &str) -> Result<usize> {
     usize::from_str_radix(s, 2).context("Failed to parse binary string!")
 }
 
-impl FreqCounter {
-    fn new(size: usize) -> Self {
-        Self {
-            size,
-            freq: vec![Counter::new(); size],
-        }
-    }
-
-    fn update(&mut self, line: &str) -> Result<()> {
-        if self.size != line.len() {
-            bail!("Counter size must match line length!")
-        }
-        line.chars()
-            .enumerate()
-            .for_each(|(i, ch)| self.freq[i][&ch] += 1);
-        Ok(())
-    }
-
-    fn gamma(&self) -> Result<String> {
-        self.freq.iter().map(mcb).collect::<Result<String>>()
-    }
-
-    fn epsilon(&self) -> Result<String> {
-        self.freq.iter().map(lcb).collect::<Result<String>>()
-    }
-}
-
 fn solve_p1() -> Result<usize> {
-    let mut f = FreqCounter::new(12);
-    get_my_lines!().try_for_each(|line| f.update(&line))?;
+    let lines: Vec<String> = get_my_lines!().collect();
+    let len = lines[0].len();
+    let gamma: Vec<u8> = (0..len)
+        .map(|i| mcb_at(&lines, i))
+        .collect::<Result<Vec<u8>>>()
+        .context("mcb failed?")?;
+    let gamma = String::from_utf8(gamma)?;
 
-    let gamma = binary_to_int(&f.gamma()?)?;
-    let epsilon = binary_to_int(&f.epsilon()?)?;
+    let epsilon: Vec<u8> = (0..len)
+        .map(|i| lcb_at(&lines, i))
+        .collect::<Result<Vec<u8>>>()
+        .context("lcb failed?")?;
+    let epsilon = String::from_utf8(epsilon)?;
+
+    let gamma = binary_to_int(&gamma)?;
+    let epsilon = binary_to_int(&epsilon)?;
     println!("gamma: {}, epsilon: {}", gamma, epsilon);
     Ok(gamma * epsilon)
 }
@@ -104,9 +70,6 @@ fn filter_by_bit(vec: &Vec<String>, cond: fn(&Vec<String>, usize) -> Result<u8>)
 
 fn solve_p2() -> Result<usize> {
     let lines: Vec<String> = get_my_lines!().collect();
-    let mut f = FreqCounter::new(12);
-    get_my_lines!().try_for_each(|line| f.update(&line))?;
-
     let oxygen = binary_to_int(&filter_by_bit(&lines, mcb_at)?)?;
     let co2 = binary_to_int(&filter_by_bit(&lines, lcb_at)?)?;
     println!("Oxygen: {}, CO2: {}", oxygen, co2);
