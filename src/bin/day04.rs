38847100165c17823278a06f0bee3dbdd0378a6d
Talha Zaman <talha@meshcapade.com>
2021-12-06 20:46:38 +0100

    day4 part1 solve

--- src/bin/day04.rs
+++ src/bin/day04.rs
@@ -1,5 +1,5 @@
 use advent::{get_my_lines, iter_lines};
-use anyhow::{bail, ensure, Context, Result};
+use anyhow::{anyhow, bail, ensure, Context, Result};
 use std::num::ParseIntError;
 
 #[derive(Debug, Default)]
@@ -9,32 +9,102 @@ struct Bingo {
 }
 
 impl Bingo {
-    fn new(it: &mut impl Iterator<Item = String>) -> Result<Self> {
+    fn new(it: &mut impl Iterator<Item = String>) -> Result<Option<Self>> {
         let mut b = Self::default();
-        ensure!(
-            it.next().context("Expected line!")? == "",
-            "Expected blank line!"
-        );
+        match it.next() {
+            Some(s) => ensure!(s == "", format!("Expected blank line, found: '{}'!", s)),
+            None => return Ok(None),
+        }
         for i in 0..5 {
-            let mut line = it.next().context("Expected line!")?;
+            let line = it
+                .next()
+                .with_context(|| format!("Expected line {} of board!", i + 1))?;
             let nums = line
                 .split_whitespace()
                 .map(|i| i.parse::<u8>())
                 .collect::<Result<Vec<u8>, ParseIntError>>()
-                .context("Failed to parse board!")?;
+                .with_context(|| format!("Failed to parse board at line: '{}'!", line))?;
 
             for j in 0..5 {
                 b.board[i][j] = nums[j];
             }
         }
-        Ok(b)
+        Ok(Some(b))
+    }
+
+    fn mark(&mut self, number: u8) -> Option<usize> {
+        for i in 0..5 {
+            for j in 0..5 {
+                if self.board[i][j] == number {
+                    self.seen[i][j] = true;
+                    return self.check();
+                }
+            }
+        }
+        None
+    }
+
+    fn check(&self) -> Option<usize> {
+        for i in 0..5 {
+            if (0..5)
+                .map(|j| self.seen[i][j])
+                .reduce(|acc, elt| acc & elt)?
+            {
+                return Some(self.unmarked_sum());
+            }
+        }
+        for j in 0..5 {
+            if (0..5)
+                .map(|i| self.seen[i][j])
+                .reduce(|acc, elt| acc & elt)?
+            {
+                return Some(self.unmarked_sum());
+            }
+        }
+        None
+    }
+
+    fn unmarked_sum(&self) -> usize {
+        let board = self.board.iter().flatten();
+        let seen = self.seen.iter().flatten();
+        Iterator::zip(board, seen)
+            .filter(|(_, &marked)| !marked)
+            .map(|(&num, _)| num as usize)
+            .sum()
     }
 }
 
-fn main() {
-    println!("Hello day4");
+fn parse_numbers(it: &mut impl Iterator<Item = String>) -> Result<Vec<u8>> {
+    it.next()
+        .ok_or(anyhow!("Bingo numbers not found!"))?
+        .split(",")
+        .map(|i| i.parse::<u8>())
+        .collect::<Result<Vec<u8>, ParseIntError>>()
+        .context("Failed to parse bingo numbers!")
+}
+
+fn solve_p1() -> Result<usize> {
     let mut lines = get_my_lines!();
-    lines.next();
-    let b = Bingo::new(&mut lines).unwrap();
-    dbg!(b);
+    let mut boards: Vec<Bingo> = Vec::new();
+    let numbers = parse_numbers(&mut lines)?;
+    loop {
+        let board = Bingo::new(&mut lines)?;
+        match board {
+            Some(board) => boards.push(board),
+            None => break,
+        }
+    }
+    for num in numbers {
+        for board in boards.iter_mut() {
+            if let Some(value) = board.mark(num) {
+                return Ok(value * num as usize);
+            }
+        }
+    }
+    bail!("No winning board found!")
+}
+
+fn main() -> Result<()> {
+    println!("Winning board value: {}", solve_p1()?);
+    Ok(())
 }
